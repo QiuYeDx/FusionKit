decbac91d0d18892c6b0ebc9d920bc51ff92297c
zihan.bai <zihan.bai@guwave.com>
2025-01-27 20:14:27 +0800

    feat: 自定义electron应用标题栏(Windows待测试)

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Tools from "./pages/Tools";
 import useInitializeTheme from "@/hook/useInitializeTheme";
 import BottomNavigation from "@/pages/components/BottomNavigation";
 import useTheme from "./hook/useTheme";
+import AppTitleBar from "./pages/components/AppTitleBar";
 
 function App() {
   // 初始化主题, 并添加系统深色模式监听
@@ -15,6 +16,11 @@ function App() {
 
   return (
     <div className="app">
+      <AppTitleBar />
+      {/* 占位用, 防止 AppTitleBar 遮挡有效内容 */}
+      {/* TODO: Windows 下的高度待确认, 可能需要为动态高度 */}
+      <div className="h-6"></div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tools" element={<Tools />} />
