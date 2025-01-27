ddd66aaf62120efb8d99dc05bdfaa8b6ebda6498
zihan.bai <zihan.bai@guwave.com>
2025-01-27 19:36:04 +0800

    fix: 修复部分代码类型 Error

--- src/pages/Setting/components/LanguageConfig.tsx
+++ src/pages/Setting/components/LanguageConfig.tsx
@@ -1,4 +1,5 @@
 import useLanguage from "@/hook/useLanguage";
+import { LangEnum } from "@/type/lang";
 import { useTranslation } from "react-i18next";
 
 function LanguageConfig() {
@@ -7,7 +8,9 @@ function LanguageConfig() {
 
   return (
     <div className="bg-base-200 p-4 rounded-lg overflow-auto">
-      <div className="text-xl font-semibold mb-4 sticky left-0">{t('setting:subtitle.language_config')}</div>
+      <div className="text-xl font-semibold mb-4 sticky left-0">
+        {t("setting:subtitle.language_config")}
+      </div>
 
       <div className="join">
         <input
@@ -16,7 +19,7 @@ function LanguageConfig() {
           name="language"
           aria-label="中文" // 固定显示“中文”
           checked={language === "zh"}
-          onChange={() => changeLanguage("zh")}
+          onChange={() => changeLanguage(LangEnum.ZH)}
         />
         <input
           className="join-item btn btn-sm bg-base-100 mt-[3px]"
@@ -24,7 +27,7 @@ function LanguageConfig() {
           name="language"
           aria-label="日本語" // 固定显示“日本語”
           checked={language === "ja"}
-          onChange={() => changeLanguage("ja")}
+          onChange={() => changeLanguage(LangEnum.JA)}
         />
         <input
           className="join-item btn btn-sm bg-base-100 mt-[3px]"
@@ -32,7 +35,7 @@ function LanguageConfig() {
           name="language"
           aria-label="English" // 固定显示“English”
           checked={language === "en"}
-          onChange={() => changeLanguage("en")}
+          onChange={() => changeLanguage(LangEnum.EN)}
         />
       </div>
     </div>
