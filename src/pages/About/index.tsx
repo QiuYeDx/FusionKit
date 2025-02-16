320e9f56a4638faec4eff22bc827fcac85dc1dbe
Bai Zihan <qiuyedx@foxmail.com>
2025-02-16 19:57:21 +0800

    refactor: 更新版本信息

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -23,7 +23,7 @@ const About: React.FC = () => {
             {t("about:subtitle.version")}
           </div>
 
-          <div>0.1.0</div>
+          <div>0.0.1</div>
         </div>
 
         <div className="bg-base-200 p-4 rounded-lg overflow-auto">
